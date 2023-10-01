89fca39ba55006d8cc88ffcd2f07067d85abcc7f
Junobee25 <wnsdhqo@naver.com>
2023-10-01 20:30:15 +0900

    Submit : 8일차 과제 제출 인터넷 로딩 완료

--- day8/assignment2/lib/home1.dart
+++ day8/assignment2/lib/home1.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: avoid_print, prefer_const_constructors, body_might_complete_normally_nullable, prefer_final_fields, unnecessary_brace_in_string_interps, unused_element
 
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:shimmer/shimmer.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
 
 class Homeone extends StatefulWidget {
   const Homeone({super.key});
@@ -14,8 +17,10 @@ class Homeone extends StatefulWidget {
 
 class _HomeoneState extends State<Homeone> {
   final dio = Dio();
+  final Connectivity _connectivity = Connectivity();
   List msgList = [];
-
+  bool isNetworkConnect = true; //네트워크 연결 상태
+  bool isCheckingNetwork = false; //네트워크 연결 확인 중
   RefreshController _refreshController = RefreshController(initialRefresh: false );
 
   Future<List> getData() async {
@@ -75,6 +80,29 @@ class _HomeoneState extends State<Homeone> {
   }
 
 
+
+void checkConnectivityNetwork() async {
+    isCheckingNetwork = true; //네트워크 연결 확인 중
+    setState(() {}); //네트워크 연결 확인 중 화면으로 그리기
+
+    final connectivityResult =
+        await (Connectivity().checkConnectivity()); //네트워크 연결 확인
+    await Future.delayed(
+        Duration(milliseconds: 1500)); // 연결 확인 중 화면을 출력하기 위한 딜레이
+
+    //네트워크가 연결된 경우
+    if (connectivityResult == ConnectivityResult.mobile ||
+        connectivityResult == ConnectivityResult.wifi) {
+      isNetworkConnect = true; //네트워크 연결 됨//데이터 가져오기
+    } else {
+      //네트워크가 연결 되지 않은 경우
+      isNetworkConnect = false; //네트워크 연결 안됨
+    }
+    isCheckingNetwork = false; //네트워크 연결 확인 종료
+    setState(() {}); //결과에 맞는 화면 그리기
+  }
+
+
   @override
   Widget build(BuildContext context) {
     print("setState");
@@ -82,7 +110,8 @@ class _HomeoneState extends State<Homeone> {
       appBar: AppBar(
         title: Text('8일차 과제'),
       ),
-      body: FutureBuilder(
+      body:!isCheckingNetwork && isNetworkConnect ?
+            FutureBuilder(
             future: _loadData(),
             builder: (BuildContext context, AsyncSnapshot snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
@@ -147,7 +176,31 @@ class _HomeoneState extends State<Homeone> {
                 return Container();
               }
             },
-          ),
+          ): Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (isCheckingNetwork) Text('인터넷이 연결 확인중입니다'), // 연결 확인 중 메세지
+                  if (!isCheckingNetwork)
+                    Text('인터넷이 연결되지 않았습니다!'), // 연결되지 않음 메세지
+                  SizedBox(height: 16),
+                  // 연결 확인중 progress
+                  if (isCheckingNetwork)
+                    SpinKitWave(
+                      color: Colors.blue,
+                      size: 32.0,
+                    ),
+                ],
+              ),
+            ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            
+            checkConnectivityNetwork();
+
+          },
+          child: Icon(Icons.wifi_find),
+        ),
     );
   }
 }
